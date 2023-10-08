faaafdc0ef8b1b8f3c28758e6e4c805b2c028b9f
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2023-10-08 10:41:37 -0400

    Add deployment configurations

--- client/src/app/config.ts
+++ client/src/app/config.ts
@@ -1,5 +1,6 @@
 const backendPort = "5000";
 const URL = `${location.protocol}//${location.hostname.replace('-4200', `-${backendPort}`)}${location.port ? `:${backendPort}` : ""}`;
+// const URL = `https://library-server-nxlnbhoqaa-uc.a.run.app`;
 
 export {
   URL
