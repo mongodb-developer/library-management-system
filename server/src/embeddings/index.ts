469b9f88cb782b0c31dcf8897daf5b5ad9408978
Joel Lord <joelphy@gmail.com>
2023-11-01 15:41:51 -0400

    Added Google Vertex  and changed serverless url

--- server/src/embeddings/index.ts
+++ server/src/embeddings/index.ts
@@ -1,4 +1,5 @@
 import getOpenAiEmbeddings from './openai.js';
+import getVertexEmbeddings from './googleVertex.js';
 import getServerlessEndpointEmbeddings from './serverlessEndpoint.js';
 
 const EMBEDDINGS_SOURCE = process.env.EMBEDDINGS_SOURCE || 'openai';
@@ -7,6 +8,8 @@ async function getTermEmbeddings(query) {
     switch (EMBEDDINGS_SOURCE) {
     case 'openai':
         return await getOpenAiEmbeddings(query);
+    case 'googleVertex':
+        return await getVertexEmbeddings(query);
     case 'serverlessEndpoint':
         return await getServerlessEndpointEmbeddings(query);
     default:
