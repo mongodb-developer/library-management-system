d1c9ff1a788eb1fad9cb77d6fe393a502192cd1e
Ricardo Mello <ricardo.mello@mongodb.com>
2025-04-03 14:35:31 -0300

    Refactoring project structure

--- java-server/src/main/java/com/mongodb/devrel/library/LibraryApplication.java
+++ java-server/src/main/java/com/mongodb/devrel/library/LibraryApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
 @Slf4j
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class LibraryApplication implements CommandLineRunner {
