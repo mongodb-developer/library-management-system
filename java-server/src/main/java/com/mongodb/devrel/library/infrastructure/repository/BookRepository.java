c5e3d6d137bb7331fca120ec538b6325be2903bb
Ricardo Mello <ricardohsmello@gmail.com>
2025-08-20 16:23:02 -0300

    refactor: adjusting classes for search lab

--- java-server/src/main/java/com/mongodb/devrel/library/infrastructure/repository/BookRepository.java
+++ java-server/src/main/java/com/mongodb/devrel/library/infrastructure/repository/BookRepository.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-
 @Repository
 public interface BookRepository extends MongoRepository<Book, String> {
     // we need to return our book.id as book._id per the JavaScript client and to be on par with the Node server
