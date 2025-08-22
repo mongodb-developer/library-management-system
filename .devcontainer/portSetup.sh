e1c4f9727b6abb918dd260f53388fb1099688fb2
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2025-08-22 09:17:01 +0300

    Revert "Added MongoDB cluster and fixed container for local development"

--- .devcontainer/portSetup.sh
+++ .devcontainer/portSetup.sh
@@ -1,19 +1,2 @@
-if [ -z "$CODESPACE_NAME" ]; then
-    echo "Not running in a codespace, exiting."
-    exit
-fi
-
-echo "Installing GH"
-
-(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) \
-&& sudo mkdir -p -m 755 /etc/apt/keyrings \
-&& out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
-&& cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
-&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
-&& sudo mkdir -p -m 755 /etc/apt/sources.list.d \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
-
-echo "Exposing ports"
-gh codespace ports visibility 4200:public 27017:public 50000:public -c $CODESPACE_NAME
+echo "Exposing port 5000"
+gh codespace ports visibility 5000:public -c $CODESPACE_NAME
