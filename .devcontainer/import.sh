e46523f351094bd057a328d8b8424ceafec1d100
Diego Freniche <diego@freniche.com>
2025-09-24 12:05:29 +0200

    Removed user / password from connection string

--- .devcontainer/import.sh
+++ .devcontainer/import.sh
@@ -2,7 +2,5 @@
 
 cd .devcontainer/data
 
-# Import Library data WITHOUT embeddings
-
-mongorestore --uri mongodb://localhost:27017/library --username admin --password mongodb  --drop --authenticationDatabase=admin library
-
+# Import Library data
+mongorestore --uri mongodb://localhost:27017/library --drop library/
