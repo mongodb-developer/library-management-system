69624b90d5496ae3d16506302e0e7910d1c57fee
juan.valencia@segurosbolivar.com-ArquitectoTIMaster <juan.valencia@segurosbolivar.com>
2025-09-26 15:29:00 -0500

    Update books.ts

--- server/src/controllers/books.ts
+++ server/src/controllers/books.ts
@@ -82,26 +82,23 @@ class BookController {
         return books[0];
     }
 
-    public async searchBooks(query: string): Promise<Book[]> {
-        const books = await collections?.books?.find(
-            {
-                $or: [
-                    {title: {$regex: new RegExp(query, 'i')}},
-                    {'authors.name': {$regex: new RegExp(query, 'i')}},
-                ]
-            }).limit(25).toArray();
-        return books;
+   public async searchBooks(query: string): Promise<Book[]> {
+  const vector = await getEmbeddings(query);
+  const aggregationPipeline = [
+    {
+      $vectorSearch: {
+        queryVector:  vector,
+        path: 'embeddings',
+        numCandidates: 100,
+        index: 'vectorsearch',
+        limit: 10,
+      }
     }
+  ];
+  const books = await collections?.books?.aggregate(aggregationPipeline).toArray() as Book[];
+  return books;
+}
 
-    public async createBook(book: Book): Promise<InsertOneResult> {
-        const result = await collections?.books?.insertOne(book);
-
-        if (!result?.insertedId) {
-            throw new Error(this.errors.UNKNOWN_INSERT_ERROR);
-        }
-
-        return result;
-    }
 
     public async updateBook(bookId: string, book: Book): Promise<UpdateResult> {
         const result = await collections?.books?.updateOne({ _id: bookId }, { $set: book });
