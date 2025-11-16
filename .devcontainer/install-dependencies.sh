ed4682ad3388848f050a15bc3b543e3860caf01f
sis0k0 <stanimira.vlaeva@gmail.com>
2025-11-16 14:04:31 +0200

    Fix ports setup

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 cd client
 npm install
 
--- .devcontainer/set-up-port.sh
+++ .devcontainer/set-up-port.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
 echo "Exposing port 5400"
 gh codespace ports visibility 5400:public -c $CODESPACE_NAME
+
+echo "Exposing port 4200"
+gh codespace ports visibility 4200:public -c $CODESPACE_NAME
