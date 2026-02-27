dc3fa88f0d16a31050a70d47b8cc6efa859683fe
sis0k0 <stanimira.vlaeva@gmail.com>
2026-02-27 14:19:42 +0200

    Add prefilter index definition

---
+++ server/src/search-indexing/prefilter.ts
@@ -0,0 +1,45 @@
+import '../load-env-vars.js';
+import { connectToDatabase, collections } from '../database.js';
+const { DATABASE_URI } = process.env;
+
+console.log('Connecting to MongoDB Atlas...');
+const client = await connectToDatabase(DATABASE_URI);
+console.log('Connected!\n');
+
+const collection = collections?.books;
+
+/**
+ * Create the Vector Search index.
+ * The index definition specifies the field that contains the vector embeddings,
+ * the number of dimensions in the vectors
+ * and the similarity metric to use for searching.
+ */
+await collection.updateSearchIndex(
+    'vectorsearch',
+    {
+        fields: [
+            {
+                type: 'vector',
+                path: 'embeddings',
+                numDimensions: 1408,
+                similarity: 'cosine'
+            },
+            {
+                type: 'filter',
+                path: '<REPLACE_WITH_YEAR_FIELD_NAME>'
+            },
+            {
+                type: 'filter',
+                path: '<REPLACE_WITH_LANGUAGE_FIELD_NAME>'
+            }
+        ],
+
+    }
+);
+
+const result = await collection.listSearchIndexes().toArray();
+console.log('Search indexes on the books collection:');
+console.log(result);
+
+await client.close();
+process.exit();
